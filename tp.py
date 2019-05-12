f54bdf54503bca49a0ea5556ac6ff7226c969f6a
ErneDainesi <50252041+ErneDainesi@users.noreply.github.com>
2019-05-12 13:31:44 -0300

    Update tp.py

--- tp.py
+++ tp.py
@@ -1,4 +1,6 @@
 #ESTE ES EL CÓDIGO DEL TP1
+Gente, soy Erne. Se me murió el celu ayer y por unos dias voy a estar sin. Asi que si quieren ir viendo el tp avisen por aca o por mail.
+
 
 def menuPrincipal():
   blablabla
