7a4b729f1777c865142bdb8fe8d968426bc89544
ErneDainesi <50252041+ErneDainesi@users.noreply.github.com>
2019-05-18 18:21:53 -0300

    Update funcionErne.py

--- funcionErne.py
+++ funcionErne.py
@@ -2,7 +2,7 @@ import random
 
 def generarUsuarios():
     dni = [41587459, 17199330, 17331253, 24748234, 31933841]
-    pin = ["-", "8491", "-", "7423", "-"]
+    pin = [" ", "8491", " ", "7423", " "]
     nomApe = ["camilo_fabregas", "ignacio_sanchez", "carla_martinez", "damian_gomez", "marta_diaz"]
     celular = ["1122503503", "1184623564", "1573861543", "1162846123", "1186847247"]
     usuarios = {}
@@ -16,7 +16,7 @@ def imprimirUsuariosBloqueados (usuarios):
     seguir = "s"
     print("**** Usuarios Bloqueados ****")
     for usuario in usuarios:
-        if usuarios[usuario][0] == "-": #Filtro del diccionario los usarios bloqueados
+        if usuarios[usuario][0] == " ": #Filtro del diccionario los usarios bloqueados
             cantidadUsuariosBloqueados += 1
             print("{}. {} con dni {}".format(cantidadUsuariosBloqueados, usuarios[usuario][1], usuario)) #imprimo indice, nombre de usuario
     return cantidadUsuariosBloqueados
@@ -33,7 +33,7 @@ def desbloquear (usuarios):
     seguir = "s"
     while seguir == "s":
         usuarioElegido = int(input("Ingrese el DNI del usuario a desbloquear: "))
-        while usuarioElegido not in usuarios or usuarios[usuarioElegido][0] != "-":
+        while usuarioElegido not in usuarios or usuarios[usuarioElegido][0] != " ":
             usuarioElegido = int(input("[ERROR] Debe ingresar el DNI de un usuario bloqueado: "))
         usuarios[usuarioElegido][0] = generarPin()
         print("[INFO] Usuario desbloqueado exitosamente. Se le generó el pin {}, a {}.".format(usuarios[usuarioElegido][0], usuarios[usuarioElegido][1]))
