d1a300090451dbce04f39cec5ef83cce1c95b415
Camilo Fábregas <50251894+MiloGNR@users.noreply.github.com>
2019-05-20 19:48:06 -0300

    Update generarEstructuras.py

--- generarEstructuras.py
+++ generarEstructuras.py
@@ -14,13 +14,23 @@ def generarBicicletas(bicicletas):
 	for num in range(1241,1251):
 		bicicletas[num] = ["Necesita reparación", "En reparación"]
 
+def generarEstaciones(estaciones, bicicletas, tipoDeCarga):
+	if estaciones:
+		estaciones.clear()
+	identificador = list(range(1,11))
+	direcciones = ["Parque Lezama", "Plaza de Mayo", "Retiro", "Facultad de Derecho", "Obelisco", "Congreso", "Constitución", "Planetario", "Parque Centenario", "Alto Palermo"]
+	latitudLongitud = [(-34.6261, -58.3684), (-34.6082, -58.3709), (-34.5916, -58.3743), (-34.5828, -58.3920), (-34.6037, -58.3814), (-34.6095, -58.3889), (-34.6266, -58.3811), (-34.5696, -58.4117), (-34.6073, -58.4335), (-34.5890, -58.4100)]
+	anclajesTotales = [25, 30, 30, 15, 30, 15, 20, 25, 20, 30]
+	anclajesOcupados = distribuirBicicletas(bicicletas, tipoDeCarga)
+	for dato1, dato2, dato3, dato4, dato5 in zip(identificador, direcciones, latitudLongitud, anclajesTotales, anclajesOcupados):
+		estaciones[dato1] = {"Dirección": dato2, "Latitud y longitud": dato3, "Capacidad": dato4, "Bicicletas": dato5}
 
-def generarEstaciones(estaciones):
-	direcciones = ["Parque Lezama", "Plaza de Mayo", "Retiro", "Facultad de Derecho", "Obelisco", "Congreso", "Constitución", "Parque Patricios", "Planetario", "Parque Centenario", "Alto Palermo"]
-	latitudLongitud = [(-34.6261, -58.3684), (-34.6082, -58.3709), (-34.5916, -58.3743), (-34.5828, -58.3920), (-34.6037, -58.3814), (-34.6095, -58.3889), (-34.6266, -58.3811), (-34.6385, -58.4060), (-34.5717, -58.4112), (-34.6073, -58.4335), (-34.5890, -58.4100)]
-	anclajesTotales = [30, 25, 20, 30, 30, 25, 30, 15, 25, 30, 25]
-	anclajesOcupados = [25, 20, 20, 25, 25, 20, 20, 15, 20, 30, 20]
-	for num in range(1,11): # Rango de identificador de la estación
-		for dato1, dato2, dato3, dato4 in zip(direcciones, latitudLongitud, anclajesTotales, anclajesOcupados):
-			estaciones[num] = {"Dirección": dato1, "Latitud y longitud": dato2, "Capacidad": dato3, "Bicicletas": dato4}
-
+def distribuirBicicletas(bicicletas, tipoDeCarga):
+	if tipoDeCarga == "predefinida":
+		print("CARGA PREDEFINIDA")
+		distribucionBicicletas = [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
+		return distribucionBicicletas
+	else:
+		print("CARGA ALEATORIA")
+		distribucionBicicletas = ["asdasdasdasd"]
+		return distribucionBicicletas
