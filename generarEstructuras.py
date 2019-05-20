ef25f153cc9464c69b02111b8d7e4b9723de9ced
agusmanduk <50275723+agusmanduk@users.noreply.github.com>
2019-05-20 19:55:41 -0300

    Update generarEstructuras.py

--- generarEstructuras.py
+++ generarEstructuras.py
@@ -20,7 +20,7 @@ def generarEstaciones(estaciones, bicicletas, tipoDeCarga):
 	identificador = list(range(1,11))
 	direcciones = ["Parque Lezama", "Plaza de Mayo", "Retiro", "Facultad de Derecho", "Obelisco", "Congreso", "Constitución", "Planetario", "Parque Centenario", "Alto Palermo"]
 	latitudLongitud = [(-34.6261, -58.3684), (-34.6082, -58.3709), (-34.5916, -58.3743), (-34.5828, -58.3920), (-34.6037, -58.3814), (-34.6095, -58.3889), (-34.6266, -58.3811), (-34.5696, -58.4117), (-34.6073, -58.4335), (-34.5890, -58.4100)]
-	anclajesTotales = [25, 30, 30, 15, 30, 15, 20, 25, 20, 30]
+	anclajesTotales = [30, 25, 20, 30, 30, 25, 30, 15, 25, 25]
 	anclajesOcupados = distribuirBicicletas(bicicletas, tipoDeCarga)
 	for dato1, dato2, dato3, dato4, dato5 in zip(identificador, direcciones, latitudLongitud, anclajesTotales, anclajesOcupados):
 		estaciones[dato1] = {"Dirección": dato2, "Latitud y longitud": dato3, "Capacidad": dato4, "Bicicletas": dato5}
