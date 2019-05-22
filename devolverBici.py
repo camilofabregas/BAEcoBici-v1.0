5f4fbb5b1c05af2cafad201f30f9380ff1db41a2
ErneDainesi <50252041+ErneDainesi@users.noreply.github.com>
2019-05-22 00:50:07 -0300

    Update devolverBici.py

--- devolverBici.py
+++ devolverBici.py
@@ -1,7 +1,7 @@
 from validaciones import*
 import random
 
-def devolverBicicleta(usuarios, estaciones):
+def devolverBicicleta(estaciones usuarios):
 	print("**** ESTACIONES ****")
 	for estacion in estaciones:
 		print("Estación {}: {}".format(estacion, estaciones[estacion]["Dirección"]))
@@ -9,14 +9,18 @@ def devolverBicicleta(usuarios, estaciones):
 	while idEstacion not in estaciones:
 		idEstacion = int(solicitarValidarDigitos(1, 10, "[ERROR]Ingrese un número de estación válido: "))
 
+	generarDuracionDeViaje(usuarios)
+
 
 def generarDuracionDeViaje(usuarios):
     num = random.choice('0123456789')
     num2 = random.choice('0123456789')
     duracionViaje = num + num2
-    if int(duracionViaje)<5:
-        generarDuracionDeviaje(usuarios)
-    elif int(duracionViaje)>60:
+    while int(duracionViaje) < 5 or int(duracionViaje) > 75:
+    	num = random.choice('0123456789')
+    	num2 = random.choice('0123456789')
+    	duracionViaje = num + num2    
+    if int(duracionViaje) > 60:
         print("[INFO] Su viaje exedió el límite de una hora. Su usuario ha sido bloqueado.")
     else:
         print("Su viaje duró {} minutos.".format(int(duracionViaje)))
