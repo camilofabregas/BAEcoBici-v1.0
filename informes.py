f040b903fb17130558b8a0965ad863bfc96bd1e7
marifortu45 <50252785+marifortu45@users.noreply.github.com>
2019-05-23 13:07:14 -0300

    Delete informes.py

--- informes.py
+++
@@ -1,9 +0,0 @@
-def bicicletasEnReparacion (bicicletas, estaciones):
-	bicisEnReparacion = []
-	for bicicleta in bicicletas:
-		if bicicletas[bicicleta][0] == "Necesita reparación":
-			bicisEnReparacion.append(bicicleta)
-	for bicicleta in bicisEnReparacion:
-		estacionBici = random.randrange(1, len(estaciones)+1)
-		print("[INFO] La bicicleta {} ubicada en la estacion {} necesita reparacion. ".format(bicicleta, estaciones[estacionBici]["Dirección"]))
-
